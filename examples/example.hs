8e35b3e7138aa4027ac2b71b68dc3de90f48bf45
dgorin <dgorin@dc.uba.ar>
2008-04-27 01:33:22 +0000

    Nicer example (catches exceptions, browse modules)

--- examples/example.hs
+++ examples/example.hs
@@ -2,11 +2,13 @@ import Control.Monad
 import Control.Monad.Trans ( liftIO )
 import Language.Haskell.Interpreter.GHC
 
+import Control.Exception ( catchDyn )
+
 main :: IO ()
 main = do s <- newSession
           withSession s testHint
           putStrLn "that's all folks"
-
+     `catchDyn` printInterpreterError
 
 testHint :: Interpreter ()
 testHint =
@@ -22,7 +24,11 @@ testHint =
       let expr1 = "(f, g, h, 42)"
       say $ "e.g. typeOf " ++ expr1
       say =<< typeOf expr1
-      say "Observe that f, g and h are defined in SomeModule.hs, but f is not exported"
+      --
+      say $ "Observe that f, g and h are defined in SomeModule.hs, " ++
+            "but f is not exported. Let's check it..."
+      exports <- getModuleExports "SomeModule"
+      say (show exports)
       --
       say "We can also evaluate an expression; the result will be a string"
       let expr2 = "length $ concat [[f,g],[h]]"
@@ -36,7 +42,8 @@ testHint =
       --
       say "This works for any monomorphic type, even for function types"
       let expr3 = "\\(Just x) -> succ x"
-      say $ "e.g. we interpret " ++ expr3 ++ " with type Maybe Int -> Int and apply it on Just 7"
+      say $ "e.g. we interpret " ++ expr3 ++
+            " with type Maybe Int -> Int and apply it on Just 7"
       fun <- interpret expr3 (as :: Maybe Int -> Int)
       say . show $ fun (Just 7)
       --
@@ -48,5 +55,9 @@ testHint =
       res <- interpret "head $ map show [\"Worked!\", \"Didn't work\"]" infer >>= flip interpret infer
       say res
 
+
 say :: String -> Interpreter ()
 say = liftIO . putStrLn
+
+printInterpreterError :: InterpreterError -> IO ()
+printInterpreterError e = putStrLn $ "Ups... " ++ (show e)
