b40b7b0cd720f7a7d7ee522227a79f0233d0a38d
jcpetruzza <jcpetruzza@gmail.com>
2008-12-08 16:33:32 +0000

    Language.Haskell.Interpreter.GHC.* renamed to Language.Haskell.Interpreter.*

--- src/Language/Haskell/Interpreter/GHC.hs
+++ src/Language/Haskell/Interpreter/GHC.hs
@@ -1,50 +1,13 @@
 -----------------------------------------------------------------------------
 -- |
--- Module      :  Language.Haskell.Interpreter.GHC
--- License     :  BSD-style
---
--- Maintainer  :  jcpetruzza@gmail.com
--- Stability   :  experimental
--- Portability :  non-portable (GHC API)
---
--- A Haskell interpreter built on top of the GHC API
+-- DEPRECATED: use @Language.Haskell.Interpreter.Unsafe@ instead.
 -----------------------------------------------------------------------------
-module Language.Haskell.Interpreter.GHC(
-    -- * The interpreter monad transformer
-     MonadInterpreter(..), InterpreterT, Interpreter,
-    -- ** Running the interpreter
-     runInterpreter,
-    -- ** Interpreter options
-     Option, OptionVal((:=)),
-     get, set,
-     languageExtensions, availableExtensions, glasgowExtensions, Extension(..),
-     installedModulesInScope,
-
-     setUseLanguageExtensions,
-     setInstalledModsAreInScopeQualified,
-    -- ** Context handling
-     ModuleName,
-     loadModules, getLoadedModules, setTopLevelModules,
-     setImports, setImportsQ,
-     reset,
-    -- ** Module querying
-     ModuleElem(..), Id, name, children,
-     getModuleExports,
-    -- ** Type inference
-     typeOf, typeChecks, kindOf,
-    -- ** Evaluation
-     interpret, as, infer, eval,
-    -- * Error handling
-     InterpreterError(..), GhcError(..),
-    -- * Miscellaneous
-     ghcVersion)
+module Language.Haskell.Interpreter.GHC
+{-# DEPRECATED "Import Language.Haskell.Interpreter instead." #-}
+(
+    module Language.Haskell.Interpreter
+)
 
 where
 
-import Hint.Base
-import Hint.InterpreterT
-import Hint.Configuration
-import Hint.Context
-import Hint.Reflection
-import Hint.Typecheck
-import Hint.Eval
+import Language.Haskell.Interpreter
---
+++ src/Language/Haskell/Interpreter/Unsafe.hs
@@ -0,0 +1,13 @@
+module Language.Haskell.Interpreter.Unsafe ( unsafeSetGhcOption )
+
+where
+
+import Hint.Base
+import Hint.Configuration
+
+-- | Set a GHC option for the current session,
+--   eg. @unsafeSetGhcOption \"-XNoMonomorphismRestriction\"@.
+--
+--   Warning: Some options may interact badly with the Interpreter.
+unsafeSetGhcOption :: MonadInterpreter m => String -> m ()
+unsafeSetGhcOption = setGhcOption
