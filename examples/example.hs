595170da4da7691c1ff5d571f27a51d066729dd6
Daniel Martí <mvdan@mvdan.cc>
2016-03-24 13:34:52 +0000

    example: use "say $" consistently

--- examples/example.hs
+++ examples/example.hs
@@ -39,28 +39,28 @@ testHint =
       say $ "Observe that f, g and h are defined in SomeModule.hs, " ++
             "but f is not exported. Let's check it..."
       exports <- getModuleExports "SomeModule"
-      say (show exports)
+      say $ show exports
       --
       say "We can also evaluate an expression; the result will be a string"
       let expr2 = "length $ concat [[f,g],[h]]"
       say $ "e.g. eval " ++ show expr2
       a <- eval expr2
-      say (show a)
+      say $ show a
       --
       say "Or we can interpret it as a proper, say, int value!"
       a_int <- interpret expr2 (as :: Int)
-      say (show a_int)
+      say $ show a_int
       --
       say "This works for any monomorphic type, even for function types"
       let expr3 = "\\(Just x) -> succ x"
       say $ "e.g. we interpret " ++ expr3 ++
             " with type Maybe Int -> Int and apply it on Just 7"
       fun <- interpret expr3 (as :: Maybe Int -> Int)
-      say . show $ fun (Just 7)
+      say $ show $ fun (Just 7)
       --
       say "And sometimes we can even use the type system to infer the expected type (eg Maybe Bool -> Bool)!"
       bool_val <- interpret expr3 infer `ap` return (Just False)
-      say (show $ not bool_val)
+      say $ show $ not bool_val
       --
       say "Here we evaluate an expression of type string, that when evaluated (again) leads to a string"
       res <- interpret "head $ map show [\"Worked!\", \"Didn't work\"]" infer >>= flip interpret infer
